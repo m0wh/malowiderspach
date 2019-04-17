f28492f14ae29771c96ba51a8c3c39911415ee29
m0wh <malo.dewiderspach@gmail.com>
2019-04-17 18:28:08 +0200

    Add nice hover effect on projects

--- src/index.js
+++ src/index.js
@@ -1,27 +1,38 @@
+import { Controller, Scene } from "scrollmagic";
+import "scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap"
+import { TweenMax } from "gsap";
+import { Menu } from "./modules/distorsion-hover"
 import { getProjects } from "./modules/data";
 
 import "./style.scss";
 
-const lerp = (start, end, amt) => (1 - amt) * start + amt * end;
+const controller = new Controller();
 
 let oldScroll = window.scrollY;
 setInterval(() => {
   const currentScroll = window.scrollY;
-  const scrollSpeed = currentScroll - oldScroll;// lerp(oldScroll, currentScroll, 0.5);
+  const scrollSpeed = currentScroll - oldScroll;
   document.querySelector("header .logo").style.fontWeight = 900 - Math.abs(scrollSpeed) * 10;
   document.querySelector("header .logo").style.transformOrigin = scrollSpeed > 0 ? "bottom left" : "top left";
   document.querySelector("header .logo").style.transform = `scale3d(1, ${1 + Math.abs(scrollSpeed)/400}, 1) skewY(${-scrollSpeed / 10}deg)`;
   oldScroll = currentScroll;
 }, 1000/60);
 
-// Get projects from API
-const projectsSection = document.querySelector("#work .projects");
-getProjects().then(projects => projects.forEach(project => projectsSection.innerHTML += formatProject(project)));
+const projectsList = document.querySelector("#work .projects");
+const coversList = document.querySelector("#work .distort .covers");
 
-const formatProject = ({ cover, name, description, url }) => `
-  <li class="project">
-    <img class="project__cover" src="${ cover }" alt="${ name }"/>
-    <a href="${ url }" target="_blank" class="project__desc"><span class="project__name">${ name } —</span> ${ description } <span class="arrow">-></span></a>
-    <!-- <a class="project__link" href="${ url }" target="_blank">${ name } on Behance <span class="arrow">-></span></a> -->
-  </li>
-`;
+// Get projects from API
+getProjects().then(projects => projects.forEach(project => {
+  const li = document.createElement("li"); // create the list item element
+  li.classList.add("project");
+  li.innerHTML = `<a href="${ project.url }" target="_blank" class="project__desc"><span class="project__name">${ project.name } —</span> ${ project.description } <span class="arrow">-></span></a>`; // add it the "project" class
+  projectsList.appendChild(li); // append it to the list element
+  coversList.innerHTML += `<image class="distort__cover" x="0" y="0" xlink:href="${ project.cover }" height="632" width="808" />`;
+  
+  // ScrollMagic & GSAP
+  new Scene({ triggerElement: li, triggerHook: .8 })
+    .setTween(TweenMax.from(li.querySelector(".project__desc"), .5, { autoAlpha: 0, y: 50 }))
+    .addTo(controller);
+})).then(() => {
+  new Menu("#work .distort", "#work .projects", ".project__desc");
+});
