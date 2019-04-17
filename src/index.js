8a08e2d3c4cf5cde86e25f08b2d46329efb5ad6b
m0wh <malo.dewiderspach@gmail.com>
2019-04-17 15:23:31 +0200

    change projects diplay

--- src/index.js
+++ src/index.js
@@ -9,25 +9,19 @@ setInterval(() => {
   const currentScroll = window.scrollY;
   const scrollSpeed = currentScroll - oldScroll;// lerp(oldScroll, currentScroll, 0.5);
   document.querySelector("header .logo").style.fontWeight = 900 - Math.abs(scrollSpeed) * 10;
-  document.querySelector("header .logo").style.transformOrigin = scrollSpeed > 0 ? "bottom" : "top";
-  document.querySelector("header .logo").style.transform = `scaleY(${1 + Math.abs(scrollSpeed)/100})`;
+  document.querySelector("header .logo").style.transformOrigin = scrollSpeed > 0 ? "bottom left" : "top left";
+  document.querySelector("header .logo").style.transform = `scale3d(1, ${1 + Math.abs(scrollSpeed)/400}, 1) skewY(${-scrollSpeed / 10}deg)`;
   oldScroll = currentScroll;
 }, 1000/60);
 
 // Get projects from API
-getProjects().then(projects => {
-  
-  const projectsSection = document.querySelector("#work .projects");
-  projects.forEach(project => {
-    const li = document.createElement("li")
-    const html = `
-      <li class="project">
-        <img class="project__cover" src="${ project.cover }" alt="${ project.name }"/>
-        <p class="project__desc"><span class="project__name">${ project.name } —</span> ${ project.description }</p>
-        <a class="project__link" href="${ project.url }" target="_blank">${ project.name } on Behance <span class="arrow">-></span></a>
-      </li>
-    `
-    projectsSection.innerHTML += html;
-  });
+const projectsSection = document.querySelector("#work .projects");
+getProjects().then(projects => projects.forEach(project => projectsSection.innerHTML += formatProject(project)));
 
-});
+const formatProject = ({ cover, name, description, url }) => `
+  <li class="project">
+    <img class="project__cover" src="${ cover }" alt="${ name }"/>
+    <a href="${ url }" target="_blank" class="project__desc"><span class="project__name">${ name } —</span> ${ description } <span class="arrow">-></span></a>
+    <!-- <a class="project__link" href="${ url }" target="_blank">${ name } on Behance <span class="arrow">-></span></a> -->
+  </li>
+`;
