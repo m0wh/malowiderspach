01db0769c31f2b41b1a19f3e6d5a26e2e6544b58
m0wh <malo.dewiderspach@gmail.com>
2019-04-17 11:46:17 +0200

    Fix layout and animation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./style.scss";
+
+const lerp = (start, end, amt) => (1 - amt) * start + amt * end;
+
+let oldScroll = window.scrollY;
+setInterval(() => {
+  const currentScroll = window.scrollY;
+  const scrollSpeed = Math.abs(currentScroll - oldScroll);// lerp(oldScroll, currentScroll, 0.5);
+  document.querySelector("header .logo").style.fontWeight = 900 - scrollSpeed * 20;
+  document.querySelector("header .logo").style.transform = `scaleY(${1 + scrollSpeed/400})`;
+  oldScroll = currentScroll;
+}, 1000/60);
+
