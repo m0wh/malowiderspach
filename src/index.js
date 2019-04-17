05b5783480d02aee89ff7c31a0f6915519fc77df
m0wh <malo.dewiderspach@gmail.com>
2019-04-17 13:40:45 +0200

    fixed logo animation and resposiveness

--- src/index.js
+++ src/index.js
@@ -7,9 +7,10 @@ const lerp = (start, end, amt) => (1 - amt) * start + amt * end;
 let oldScroll = window.scrollY;
 setInterval(() => {
   const currentScroll = window.scrollY;
-  const scrollSpeed = Math.abs(currentScroll - oldScroll);// lerp(oldScroll, currentScroll, 0.5);
-  document.querySelector("header .logo").style.fontWeight = 900 - scrollSpeed * 20;
-  document.querySelector("header .logo").style.transform = `scaleY(${1 + scrollSpeed/400})`;
+  const scrollSpeed = currentScroll - oldScroll;// lerp(oldScroll, currentScroll, 0.5);
+  document.querySelector("header .logo").style.fontWeight = 900 - Math.abs(scrollSpeed) * 10;
+  document.querySelector("header .logo").style.transformOrigin = scrollSpeed > 0 ? "bottom" : "top";
+  document.querySelector("header .logo").style.transform = `scaleY(${1 + Math.abs(scrollSpeed)/100})`;
   oldScroll = currentScroll;
 }, 1000/60);
 
